f6fc6a8411784884839e9fa52e4527483c16d73a
Toan Vo <toanalien@gmail.com>
2020-10-06 15:39:24 +0000

    Add 1 side orders

--- gaction.py
+++ gaction.py
@@ -74,13 +74,9 @@ dict_ticker_price = {
 }
 
 symbol = sys.argv[1].upper()
-quantity = float(sys.argv[2])
-step = float(sys.argv[3])
-max_grid = int(sys.argv[4])
-side = None
-side_effect_type = None
 
 current_price = dict_ticker_price[symbol]["last"]
+
 filters = dict_market[symbol]['info']['filters']
 PRICE_FILTER = list(
     filter(lambda x: x['filterType'] == 'PRICE_FILTER', filters)).pop()
@@ -89,38 +85,99 @@ PRICE_FILTER_VAL = float(PRICE_FILTER['minPrice'])
 
 precision = precision_and_scale(float(PRICE_FILTER_VAL))[-1]
 
-for i in range(max_grid * -1, max_grid + 1):
-    if i == 0:
-        continue
-    if i < 0:
-        side = "BUY"
-        side_effect_type = 'MARGIN_BUY'
-    else:
-        side = "SELL"
+if len(sys.argv) == 5:
+    quantity = float(sys.argv[2])
+    step = float(sys.argv[3])
+    max_grid = int(sys.argv[4])
+    side = None
+    side_effect_type = None
+
+    for i in range(max_grid * -1, max_grid + 1):
+        if i == 0:
+            continue
+        if i < 0:
+            side = "BUY"
+            side_effect_type = 'MARGIN_BUY'
+        else:
+            side = "SELL"
+            side_effect_type = 'NO_SIDE_EFFECT'
+
+        price = current_price * (100 + i) / 100
+
+        price = round(price, precision)
+        print("{} {} at {}".format(side, quantity, round(price, precision)))
+
+        exchange.sapi_post_margin_loan({
+            'asset': symbol.replace('USDT', ''),
+            'amount': quantity
+        })
+
+        order = exchange.sapi_post_margin_order({
+            'symbol':
+            symbol,
+            'side':
+            side,
+            'quantity':
+            quantity,
+            'price':
+            price,
+            'type':
+            'LIMIT',
+            'timeInForce':
+            'GTC',
+            "sideEffectType":
+            side_effect_type
+        })
+
+        pp.pprint(order)
+        time.sleep(3)
+elif len(sys.argv) == 6:
+    print("single side")
+    side = sys.argv[2].upper()
+    quantity = float(sys.argv[3])
+    step = float(sys.argv[4])
+    max_grid = int(sys.argv[5])
+    side_effect_type = None
+
+    if side == 'SELL':
+        _range = range(0, max_grid + 1)
         side_effect_type = 'NO_SIDE_EFFECT'
+    elif side == 'BUY':
+        _range = range(max_grid * -1, 0)
+        side_effect_type = 'MARGIN_BUY'
 
-    price = current_price * (100 + i) / 100
+    for i in _range:
+        price = current_price * (100 + i) / 100
 
-    price = round(price, precision)
-    print("{} {} at {}".format(side, quantity, round(price, precision)))
+        price = round(price, precision)
+        print("{} {} at {}".format(side, quantity, round(price, precision)))
 
-    exchange.sapi_post_margin_loan({
-        'asset': symbol.replace('USDT', ''),
-        'amount': quantity
-    })
+        exchange.sapi_post_margin_loan({
+            'asset': symbol.replace('USDT', ''),
+            'amount': quantity
+        })
 
-    order = exchange.sapi_post_margin_order({
-        'symbol': symbol,
-        'side': side,
-        'quantity': quantity,
-        'price': price,
-        'type': 'LIMIT',
-        'timeInForce': 'GTC',
-        "sideEffectType": side_effect_type
-    })
+        order = exchange.sapi_post_margin_order({
+            'symbol':
+            symbol,
+            'side':
+            side,
+            'quantity':
+            quantity,
+            'price':
+            price,
+            'type':
+            'LIMIT',
+            'timeInForce':
+            'GTC',
+            "sideEffectType":
+            side_effect_type
+        })
 
-    pp.pprint(order)
-    time.sleep(3)
+        pp.pprint(order)
+        time.sleep(3)
 
 # python gaction.py symbol quantity_per_order spread total_orders_per_side
 # python gaction.py dashusdt 0.2 1 5 (total 5*2 = 10 orders)
+
+print(len(sys.argv))
