7763e54c404d0a8cea67e587664424ad1c6e38f6
Toan Vo <toanalien@gmail.com>
2020-10-07 17:23:22 +0000

    Concept

--- volatility-handle.py
+++ volatility-handle.py
@@ -1,1 +1,12 @@
-import ccxt
+"""
+Concept:
+- When bearish market:
+    + Cancel all orders
+    + Sell all free assets remaining; dont care borrowed
+
+- When bullish market:
+    + Cancel all orders
+    + Calculate which assets borrowed
+        + If free >= borrow -> repay
+        + If free < borrow; buy market, amount = borrow - free; -> repay
+"""
